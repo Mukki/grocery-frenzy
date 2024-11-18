c5eccd70ba2d50d5795b3e99244daca2ea3dc4b8
Juba Hammadou <thetank62200@gmail.com>
2024-11-18 12:23:36 -0500

    update from main

---
+++ Source/GroceryFrenzy/Private/Hazards/FallingTile.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Hazards/FallingTile.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AFallingTile::AFallingTile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+
+	StaticMesh->SetupAttachment(Collider);
+}
+
+// Called when the game starts or when spawned
+void AFallingTile::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AFallingTile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFallingTile::Trigger()
+{
+	Collider->SetEnableGravity(true);
+	Collider->SetSimulatePhysics(true);
+	IsActive = true;
+}
+
+
---
+++ Source/GroceryFrenzy/Public/Hazards/HazardManager.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HazardManager.generated.h"
+
+UCLASS()
+class GROCERYFRENZY_API AHazardManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BaseDelayBetweenHazard = 10.0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DelayDecrease = 1.0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinDelayBetweenHazard = 2.0;
+
+	// Sets default values for this actor's properties
+	AHazardManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Triggers a randomly choosen hazard in the level
+	UFUNCTION()
+	void TriggerRandomHazard();
+
+	UPROPERTY()
+	float CurrentDelayBetweenHazard;
+	UPROPERTY()
+	FTimerHandle HazardTimerHandle;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
